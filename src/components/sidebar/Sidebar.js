0b95e192b3967ed1d9b7331b4867d6862a8b59a6
vladhuk <vladhookovskiy@gmail.com>
2019-11-24 01:00:26 +0200

    Issue "Page layout" - Fix sidebar style;

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,10 +1,13 @@
 import React from "react";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import './Sidebar.css'
 
 function Sidebar(props) {
-    return <Navbar className='col-md-2 border bg-light'>
-        <Nav className='min-vh-100 flex-column w-100'>
+    return <Navbar bg='light'
+                   className='col-md-2 border-right flex-column sidebar'
+    >
+        <Nav className='flex-column w-100'>
             {props.children}
         </Nav>
     </Navbar>

6faf1b1ac14bc37e388fbc9884fcbc590284fbc5
vladhuk <vladhookovskiy@gmail.com>
2019-12-15 01:36:25 +0200

    Issue "Import api" - Add token provider;

---
+++ src/util/access-token-provider.js
@@ -0,0 +1,16 @@
+import Cookies from 'universal-cookie';
+import {ACCESS_TOKEN_COOKIE_NAME} from "../constants";
+
+const cookies = new Cookies();
+
+export function getToken() {
+    return cookies.get(ACCESS_TOKEN_COOKIE_NAME);
+}
+
+export function setToken(token) {
+    cookies.set(ACCESS_TOKEN_COOKIE_NAME, token);
+}
+
+export function deleteToken() {
+    cookies.remove(ACCESS_TOKEN_COOKIE_NAME);
+}

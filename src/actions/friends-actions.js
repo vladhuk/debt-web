94a6923bd91c4bab71d87cbcce41047f01233c33
vladhuk <vladhookovskiy@gmail.com>
2019-12-15 01:38:06 +0200

    Issue "Import api" - Refactor;

--- src/actions/friends-actions.js
+++ src/actions/friends-actions.js
@@ -1,3 +1,5 @@
+import {get} from "../api";
+
 export const GET_ALL_FRIENDS = 'friends:get';
 
 export function getAllFriends(friends) {
@@ -10,15 +12,8 @@ export function getAllFriends(friends) {
 }
 
 export function getAllFriendsRequest() {
-    return dispatch => {
-        fetch('http://localhost:8080/api/friends', {
-            headers: {
-                'Authorization': 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxMDg3IiwiaWF0IjoxNTc2MzU3OTExLCJleHAiOjE1NzY5NjI3MTF9.VOEFU9uvo-ln1wvhfgOYtMAu2O3ep7q_M1cJQnw42O8wfNJnXcwaDSn8Gea3iiH78cZ3b_eGeT8mafa0rOs9aw'
-            }
-        })
-            .then(response => response.json())
-            .then(friends => {
-                dispatch(getAllFriends(friends))
-            });
-    }
+    return dispatch => get({
+        resourcePath: '/friends',
+        onSuccess: friends => dispatch(getAllFriends(friends))
+    });
 }

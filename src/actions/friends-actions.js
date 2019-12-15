c394e18d67efae7bba8d767a7076db2ed92247e5
vladhuk <vladhookovskiy@gmail.com>
2019-12-15 02:23:56 +0200

    Issue "Import api" - Rename type;

--- src/actions/friends-actions.js
+++ src/actions/friends-actions.js
@@ -1,6 +1,6 @@
 import {getData} from "../api";
 
-export const GET_ALL_FRIENDS = 'friends:getData';
+export const GET_ALL_FRIENDS = 'friends:getAll';
 
 export function getAllFriends(friends) {
     return {

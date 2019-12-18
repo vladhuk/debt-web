9563e449882ff315cf22fb6e678db1a31d89e9ed
vladhuk <vladhookovskiy@gmail.com>
2019-12-18 19:14:30 +0200

    Issue "Page layout" - Add groups page;

--- src/components/Navigation/NavigationPage.js
+++ src/components/Navigation/NavigationPage.js
@@ -1,13 +1,13 @@
 import React from "react";
 import {Route, Switch} from "react-router-dom";
-import {DebtsPage, FriendsPage, PageSignIn, PageSignUp} from "../Page";
+import {DebtsPage, FriendsPage, PageGroups, PageSignIn, PageSignUp} from "../Page";
 
 
 function NavigationPage() {
     return <Switch>
         <Route exact path="/" component={null}/>
         <Route path="/friends" component={FriendsPage}/>
-        <Route path="/groups" component={null}/>
+        <Route path="/groups" component={PageGroups}/>
         <Route path="/debts" component={DebtsPage}/>
         <Route exact path="/signin" component={PageSignIn}/>
         <Route exact path="/signup" component={PageSignUp}/>

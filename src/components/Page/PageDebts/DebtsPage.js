37d6720298e59eded7b2376ff30fee9f42c7dd1d
vladhuk <vladhookovskiy@gmail.com>
2019-12-21 14:36:41 +0200

    Issue "Page layout" - Connect tabs to debts page;

--- src/components/Page/PageDebts/DebtsPage.js
+++ src/components/Page/PageDebts/DebtsPage.js
@@ -1,11 +1,13 @@
 import React from "react";
 import {BrowserRouter} from "react-router-dom";
 import DebtsPageSidebar from "./DebtsPageSidebar";
+import {NavigationDebtsSidebar} from "../../Navigation";
 
 
 function DebtsPage() {
     return <BrowserRouter>
         <DebtsPageSidebar/>
+        <NavigationDebtsSidebar/>
     </BrowserRouter>
 }
 

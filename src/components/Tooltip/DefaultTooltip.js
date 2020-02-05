eddbb4165b44f72d358a7060867b8799792f5ed0
vladhuk <vladhookovskiy@gmail.com>
2020-02-05 23:26:54 +0200

    Issue "Page layout" - Add default tooltip component;

---
+++ src/components/Tooltip/DefaultTooltip.js
@@ -0,0 +1,11 @@
+import React from "react";
+import {OverlayTrigger, Tooltip} from "react-bootstrap";
+
+export function DefaultTooltip({text, children}) {
+    return <OverlayTrigger
+        overlay={<Tooltip id={'default-tooltip' + text}>{text}</Tooltip>}
+        delay={{show: 400}}
+    >
+        {children}
+    </OverlayTrigger>
+}

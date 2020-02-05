8a5e5a1ad5b2f8407da307c2a8f2dc5ac4970517
vladhuk <vladhookovskiy@gmail.com>
2020-02-05 23:29:13 +0200

    Issue "Page layout" - Add tooltips to status icons;

--- src/components/Card/CardHeader/util.js
+++ src/components/Card/CardHeader/util.js
@@ -1,15 +1,16 @@
 import {Icon, IconAccepted, IconRejected, IconViewed} from "../../Icon";
 import React from "react";
 import {STATUS} from "../../../constants";
+import {DefaultTooltip} from "../../Tooltip";
 
 export function getStatusIcon(status) {
     switch (status) {
         case STATUS.VIEWED:
-            return <IconViewed/>;
+            return <DefaultTooltip text='Viewed'><IconViewed/></DefaultTooltip>;
         case STATUS.ACCEPTED:
-            return <IconAccepted/>;
+            return <DefaultTooltip text='Accepted'><IconAccepted/></DefaultTooltip>;
         case STATUS.REJECTED:
-            return <IconRejected/>;
+            return <DefaultTooltip text='Rejected'><IconRejected/></DefaultTooltip>;
         default:
             return <Icon className='opacity-0'/>;
     }

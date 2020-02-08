40bce24e5906933cce6288ead76bb056c79289fe
vladhuk <vladhookovskiy@gmail.com>
2020-02-09 01:02:39 +0200

    Issue "Page layout" - Fix link clicking on card header;

--- src/components/Card/CardHeader/CardLinkHeader.js
+++ src/components/Card/CardHeader/CardLinkHeader.js
@@ -4,10 +4,14 @@ import React from "react";
 export function CardLinkHeader(props) {
     return <Toast.Header
         className='link-header'
-        onClick={props.onTitleClick}
         closeButton={props.closeButton && true}
     >
-        <h4 className={'text-dark ' + (props.username ? 'mr-1' : 'mr-auto')}>{props.name}</h4>
+        <h4
+            className={'text-dark ' + (props.username ? 'mr-1' : 'mr-auto')}
+            onClick={props.onTitleClick}
+        >
+            {props.name}
+        </h4>
         {
             props.username && <div className='text-secondary mr-auto'>(@{props.username})</div>
         }

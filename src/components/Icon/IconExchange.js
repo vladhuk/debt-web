2d5188dfcf558cb75c938ac04fbce2c076a4469b
vladhuk <vladhookovskiy@gmail.com>
2020-02-15 21:06:38 +0200

    Issue debt-web#5: "Improve validation of forms" - Add new icon;

---
+++ src/components/Icon/IconExchange.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import {Icon} from "./Icon";
+import image from './../../assets/icons/exchange.svg';
+
+export function IconExchange(props) {
+    return <Icon className='opacity-50' src={image} {...props} />;
+}

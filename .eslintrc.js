4ff6ca122b817064336e3a71cbe0762b281b54c3
vladhuk <vladhookovskiy@gmail.com>
2020-02-21 11:47:03 +0200

    Issue debt-web#9: "Add utilities and run it" - Improve eslint and flow config;

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,7 @@
+const OFF = 0;
+const WARN = 1;
+const ERROR = 2;
+
 module.exports = {
   parser: 'babel-eslint',
 
@@ -12,6 +16,26 @@ module.exports = {
   ],
 
   rules: {
-    'prettier/prettier': ['error'],
+    'prettier/prettier': [WARN],
+
+    'import/prefer-default-export': OFF,
+    'import/order': WARN,
+    'import/no-useless-path-segments': WARN,
+
+    'react/jsx-filename-extension': OFF,
+    'react/destructuring-assignment': WARN,
+    'react/prop-types': OFF,
+    'react/jsx-boolean-value': WARN,
+    'react/jsx-props-no-spreading': ERROR,
+    'react/jsx-one-expression-per-line': OFF,
+
+    'jsx-a11y/alt-text': WARN,
+
+    'object-shorthand': WARN,
+    'no-use-before-define': WARN,
+    'prefer-template': WARN,
+    'no-unused-expressions': OFF,
+    'no-unused-vars': WARN,
+    'no-undef': OFF,
   }
 };

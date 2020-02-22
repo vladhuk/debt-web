7a8ec20d786294f26f66d39d31d40d4367bc61da
vladhuk <vladhookovskiy@gmail.com>
2020-02-23 01:41:11 +0200

    Issue debt-web#11: "Refactor code according to ESLint and add flow-types" - Add flow types to access-token-provider;

--- src/util/access-token-provider.js
+++ src/util/access-token-provider.js
@@ -1,16 +1,18 @@
+// @flow
+
 import Cookies from 'universal-cookie';
 import { ACCESS_TOKEN_COOKIE_NAME } from '../constants';
 
 const cookies = new Cookies();
 
-export function getToken() {
+export function getToken(): string {
   return cookies.get(ACCESS_TOKEN_COOKIE_NAME);
 }
 
-export function setToken(token) {
+export function setToken(token: string): void {
   cookies.set(ACCESS_TOKEN_COOKIE_NAME, token, { path: '/' });
 }
 
-export function deleteToken() {
+export function deleteToken(): void {
   cookies.remove(ACCESS_TOKEN_COOKIE_NAME, { path: '/' });
 }

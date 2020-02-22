da2d8908bab4725eee07d2879c270d89b1e113be
vladhuk <vladhookovskiy@gmail.com>
2020-02-23 01:39:09 +0200

    Issue debt-web#11: "Refactor code according to ESLint and add flow-types" - Add new constants;

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,11 @@
 export const API_BASE_URL = 'http://localhost:8080/api';
 export const ACCESS_TOKEN_COOKIE_NAME = 'AccessToken';
+export const HTTP_METHOD = {
+  GET: 'GET',
+  POST: 'POST',
+  PUT: 'PUT',
+  DELETE: 'DELETE',
+};
 export const STATUS = {
   SENT: 'SENT',
   VIEWED: 'VIEWED',

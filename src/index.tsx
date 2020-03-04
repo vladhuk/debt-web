362014da8301f88da7d0b64593452d9e805d0f0d
vladhuk <vladhookovskiy@gmail.com>
2020-03-05 01:33:03 +0200

    Issue debt-web#11: "Refactor code according to ESLint and add flow-types [Updated: Typescript instead of flow]" - Migrate react root component to ts;

--- src/index.tsx
+++ src/index.tsx
@@ -4,16 +4,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import App from './components/App';
-import {applyMiddleware, compose, createStore} from 'redux';
-import {Provider} from 'react-redux';
+import { applyMiddleware, compose, createStore } from 'redux';
+import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
+import App from './components/App';
 import rootReducer from './reducers/root-reducer';
 
-const allStoreEnchanters = compose(
-  applyMiddleware(thunk),
-  window.devToolsExtension && window.devToolsExtension()
-);
+export const composeEnhancers =
+  (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const allStoreEnchanters = composeEnhancers(applyMiddleware(thunk));
 
 const store = createStore(rootReducer, allStoreEnchanters);
 

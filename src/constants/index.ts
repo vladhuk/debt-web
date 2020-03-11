99e3209753e28bed14e93a9a8bf142b9a326af31
vladhuk <vladhookovskiy@gmail.com>
2020-03-11 18:48:13 +0200

    Issue debt-web#11: "Refactor code according to ESLint and add flow-types [Updated: Typescript instead of flow]" - Add type to constant;

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,5 @@
+import { StatusName } from '../types/model/Status';
+
 export const API_BASE_URL = 'http://localhost:8080/api';
 export const ACCESS_TOKEN_COOKIE_NAME = 'AccessToken';
 export const HTTP_METHOD = {
@@ -6,7 +8,12 @@ export const HTTP_METHOD = {
   PUT: 'PUT',
   DELETE: 'DELETE',
 };
-export const STATUS = {
+export const STATUS: {
+  SENT: StatusName;
+  VIEWED: StatusName;
+  ACCEPTED: StatusName;
+  REJECTED: StatusName;
+} = {
   SENT: 'SENT',
   VIEWED: 'VIEWED',
   ACCEPTED: 'ACCEPTED',

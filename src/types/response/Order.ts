a62ea84ed710002765d009d96784e07ac7680f3a
vladhuk <vladhookovskiy@gmail.com>
2020-03-15 01:20:48 +0200

    Issue debt-web#11: "Refactor code according to ESLint and add flow-types [Updated: Typescript instead of flow]" - Fix type;

--- src/types/response/Order.ts
+++ src/types/response/Order.ts
@@ -1,7 +1,9 @@
 import { Status } from './Status';
+import { User } from './User';
 
 export interface Order {
   readonly id: number;
   readonly amount: number;
   readonly status: Status;
+  readonly receiver: User;
 }
